e471b2aa21fbdc29435a6d69fa469451114c6c2e
veee676 <veesharma67@gmail.com>
2026-04-24 15:10:36 -0500

    fixed rag function

--- ai_crash_agent/rag/rag_pipeline.py
+++ ai_crash_agent/rag/rag_pipeline.py
@@ -1,22 +1,26 @@
 import ollama
 
 def generate_explanation(input_data, severity, injuries):
-    prompt = f'''
-    You are a traffic safety expert.
+    try:
+        prompt = f'''
+        You are a traffic safety expert.
 
-    A crash occurred with:
-    {input_data}
+        A crash occurred with:
+        {input_data}
 
-    Model predictions:
-    - Crash Severity: {severity}
-    - Injury Count: {injuries}
+        Model predictions:
+        - Crash Severity: {severity}
+        - Injury Count: {injuries}
 
-    Explain why this crash likely resulted in this severity.
-    '''
+        Explain why this crash likely resulted in this severity.
+        '''
 
-    response = ollama.chat(
-        model='llama3',
-        messages=[{'role': 'user', 'content': prompt}]
-    )
+        response = ollama.chat(
+            model='phi',
+            messages=[{'role': 'user', 'content': prompt}]
+        )
 
-    return response['message']['content']
+        return response['message']['content']
+    
+    except Exception as e:
+        return f"LLM Unavailable: {str(e)}"
